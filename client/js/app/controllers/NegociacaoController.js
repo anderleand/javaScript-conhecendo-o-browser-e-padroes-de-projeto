75d3b52561b4ddb0f6a3bfa3dd3de82bb1d9dc48
anderleand <andersonleandro.desouza@gmail.com>
2021-03-27 23:25:18 -0300

    Update NegociacaoController.js
    Substituição da function por arrow function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -19,18 +19,17 @@ class NegociacaoController {
         let data = new Date(...
             this._inputData.value
             .split('-')
-            .map( function (item, indice){
-                    return item - indice % 2;
-            })
+            .map((item, indice) => item - indice % 2)
         );
         //let data = new Date(this._inputData.value.replace(/-/g, ','));
 
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     this._inputQuantidade.value,
-        //     this._inputValor.value
-        // );
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
         console.log(data);
+        console.log(negociacao);
         //adicionar a negociacao em uma lista
 
     }
