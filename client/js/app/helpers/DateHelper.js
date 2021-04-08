59d2f1a0124db7ae576d5c1635542a9faa1aa19c
anderleand <andersonleandro.desouza@gmail.com>
2021-04-08 14:50:49 -0300

    Adicionado comentário explicando a subtração realizada no mês com o uso do mod

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -15,6 +15,6 @@ class DateHelper {
         if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
             throw new Error('Deve estar no formato aaaa-mm-dd');
              
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); // Subtrai o mod de 2 com base no ídice do array[0,1,2] (Vai aplicar apenas para o índice 1 que é o array referente ao mês) 
     }
 }
