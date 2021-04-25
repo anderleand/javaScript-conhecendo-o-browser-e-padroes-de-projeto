4107db2791919ef98d12fda0452ad43973032776
anderleand <andersonleandro.desouza@gmail.com>
2021-04-25 18:34:00 -0300

    Update NegociacaoDao.js
    Refatorando

--- client/js/app/dao/NegociacaoDao.js
+++ client/js/app/dao/NegociacaoDao.js
@@ -6,9 +6,10 @@ class NegociacaoDao {
 
     adiciona(){
         return new Promise((resolve, reject) => {
-            let transaction = this._connection.transaction([this._store], 'readwrite');
-            let store = this._store.objectStore(this._store);
-            let request = this._store.add(this._store);
+            let request = this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store)
+                .add(this._store);
 
             request.onsuccess = e=>{
                 resolve();
